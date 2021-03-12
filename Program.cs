74f595d8d319589375fd9f6e4998fad625162866
paulustk <paulus.kusumo@gmail.com>
2021-03-12 14:01:55 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POCPDFPwd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
